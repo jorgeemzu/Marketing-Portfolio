578174c7b8ec4cd55f856f34a4ee8999220da840
jorgeemzu <jorge.emzu@gmail.com>
2020-12-03 20:05:56 -0430

    se agrego seccion de blog y pagina "nosotros". tambien se agrego lazy loading usando intersection observers en app.js tambien se modifico el texto en la animacion de inicio ajustando el tamaño y alineandolo a la izquierda

---
+++ animation.js
@@ -0,0 +1,22 @@
+let tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+
+
+tl.to('.anim__h1', {opacity: 1, duration: 0.5})
+tl.to('.anim__h1', {opacity: 0, duration: 0.5, delay: 3})
+tl.to('.bg__green', {y:'100%', duration: 1, }) 
+tl.to('.bg__black', {y:'-100%', duration: 1}, "-=1")
+tl.from('.hero__text h1',{x: '-40px', opacity: 0, duration: 1})
+tl.from('.hero__text p',{x: '-40px', opacity: 0, duration: 1})
+tl.from('.btn',{x: '-40px', opacity: 0, duration: 0.5})
+tl.from('.hero__img',{x: '40px', opacity: 0, duration: 1}, "-=3")
+tl.from('nav',{y: '-40px', opacity: 0, duration: 1, delay: 0.5})
+tl.to('.nav__menu', {opacity:1})
+tl.to('.bg__green', {scale: 0, duration: 1, } )
+tl.to('.bg__black', {scale: 0, duration: 1, } )
+tl.to('.anim__bg', {scale: 0, duration: 1, } )
+
+tl.set('.hero__img', {clearProps: 'all'});
+tl.set('.hero__text h1', {clearProps: 'all'});
+tl.set('.hero__tet p', {clearProps: 'all'});
+tl.set('.btn', {clearProps: 'all'});
+tl.set('nav', {clearProps: 'all'});
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.hamburger__menu');
 const navMenu = document.querySelector('.nav__menu');
 const iconX = document.querySelector('.icon__x');
+const fadeLeft = document.querySelectorAll('.fade__left')
+const fadeRight = document.querySelectorAll('.fade__right')
+const fadeIn = document.querySelectorAll('.fadein')
 
 
 hamburger.addEventListener('click', () =>{
@@ -11,28 +14,25 @@ iconX.addEventListener('click', () =>{
     navMenu.classList.toggle('showMenu');   
 })
 
-let tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
+const appearOptions = {
+    rootMargin: "0px 0px -200px 0px"
+};
+const appearOnScroll = new IntersectionObserver(
+    (entries, appearOnScroll) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting){
+                return;
+            } else{
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    },
+    appearOptions);
 
-tl.to('.anim__h1', {opacity: 1, duration: 0.5})
-tl.to('.anim__h1', {opacity: 0, duration: 0.5, delay: 3})
-tl.to('.bg__green', {y:'100%', duration: 1, }) 
-tl.to('.bg__black', {y:'-100%', duration: 1}, "-=1")
-tl.from('.hero__text h1',{x: '-40px', opacity: 0, duration: 1})
-tl.from('.hero__text p',{x: '-40px', opacity: 0, duration: 1})
-tl.from('.btn',{x: '-40px', opacity: 0, duration: 0.5})
-tl.from('.hero__img',{x: '40px', opacity: 0, duration: 1}, "-=3")
-tl.from('nav',{y: '-40px', opacity: 0, duration: 1, delay: 0.5})
-tl.to('.nav__menu', {opacity:1})
-tl.to('.bg__green', {scale: 0, duration: 1, } )
-tl.to('.bg__black', {scale: 0, duration: 1, } )
-tl.to('.anim__bg', {scale: 0, duration: 1, } )
-
-tl.set('.hero__img', {clearProps: 'all'});
-tl.set('.hero__text h1', {clearProps: 'all'});
-tl.set('.hero__tet p', {clearProps: 'all'});
-tl.set('.btn', {clearProps: 'all'});
-tl.set('nav', {clearProps: 'all'});
-
+    fadeLeft.forEach( fader => appearOnScroll.observe(fader));
+    fadeRight.forEach( fader => appearOnScroll.observe(fader));
+    // fadeIn.forEach(fader => appearOnScroll.observe(fader));
 
  
